367e6b5605608f2018c574b578b4b425c0178f88
RaghavaD542 <raghavadonthoju@gmail.com>
2023-07-03 02:12:19 +0530

    updated frontend

--- src/EmployeeDetail.jsx
+++ src/EmployeeDetail.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import './EmployeeDetail.css';
 
 function EmployeeDetail() {
   const { id } = useParams();
@@ -34,31 +35,93 @@ function EmployeeDetail() {
   };
   return (
     <div>
-      <div className="d-flex justify-content-center flex-column align-items-center mt-3">
-        {employee.image && (
-          <img
-            src={`http://localhost:8081/images/` + employee.image}
-            alt=""
-            className="empImg"
-          />
-        )}
-        <div className="d-flex flex-column mt-5">
-          <h3>Name: {employee.name}</h3>
-          <h3>Email: {employee.email}</h3>
-          <h3>Salary: {employee.salary}</h3>
-          <h3>Work: {employee.work}</h3>
-          <h3>Status: {employee.status === 0 ? '⏳' : '✅'}</h3>
-        </div>
-        <button onClick={handleSubmit} className="btn btn-success">
-          Work Finished
-        </button>
-        <br />
-        <div>
-          <button className="btn btn-danger" onClick={handleLogout}>
-            Logout
-          </button>
-        </div>
+      <div class="area">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
       </div>
+      <section className="context">
+        <div className="container py-5 h-100">
+          <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="col col-lg-6 mb-4 mb-lg-0">
+              <div className="card mb-3" style={{ borderRadius: '.5rem' }}>
+                <div className="row g-0">
+                  <div
+                    className="col-md-4 gradient-custom text-center text-white"
+                    style={{
+                      borderTopLeftRadius: '.5rem',
+                      borderBottomLeftRadius: '.5rem',
+                    }}
+                  >
+                    <img
+                      src={`http://localhost:8081/images/` + employee.image}
+                      alt="Avatar"
+                      className="img-fluid my-5"
+                      style={{ width: '150px' }}
+                    />
+                    <h5>{employee.name}</h5>
+                    <p>{employee.address}</p>
+                    <i className="far fa-edit mb-5"></i>
+                  </div>
+                  <div className="col-md-8">
+                    <div className="card-body p-4">
+                      <h6>Information</h6>
+                      <hr className="mt-0 mb-4" />
+                      <div className="row pt-1">
+                        <div className="col-6 mb-3">
+                          <h6>Email</h6>
+                          <p className="text-muted">{employee.email}</p>
+                        </div>
+                        <div className="col-6 mb-3">
+                          <h6>Salary</h6>
+                          <p className="text-muted">{employee.salary}</p>
+                        </div>
+                      </div>
+                      <h6>Projects</h6>
+                      <hr className="mt-0 mb-4" />
+                      <div className="row pt-1">
+                        <div className="col-6 mb-3">
+                          <h6>Work</h6>
+                          <p className="text-muted">{employee.work}</p>
+                        </div>
+                        <div className="col-6 mb-3">
+                          <h6>Status</h6>
+                          <p className="text-muted">
+                            {employee.status === 0 ? '⏳' : '✅'}
+                          </p>
+                        </div>
+                      </div>
+                      <div className="d-flex justify-content-between mt-4">
+                        <button
+                          onClick={handleSubmit}
+                          className="btn btn-success"
+                        >
+                          Work Finished
+                        </button>
+                        <button
+                          className="btn btn-danger"
+                          onClick={handleLogout}
+                        >
+                          Logout
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/Start.jsx
+++ src/Start.jsx
@@ -1,28 +1,47 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Start.css';
 
 function Start() {
   const navigate = useNavigate();
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 loginPage">
-      <div className="p-3 rounded w-25 border loginForm text-center">
-        <h2 style={{ color: 'black' }}>Login As</h2>
-        <div className="d-flex justify-content-between mt-5">
-          <button
-            className="btn btn-primary btn-lg"
-            onClick={(e) => navigate('/employeeLogin')}
+    <section id="hero" className="hero d-flex align-items-center">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 d-flex flex-column justify-content-center">
+            <h1 data-aos="fade-up">Workforce Administration Platform</h1>
+            <div>
+              <h5 data-aos="fade-up">Login as</h5>
+            </div>
+            <div data-aos="fade-up" data-aos-delay="600">
+              <div className="text-center text-lg-start">
+                <button
+                  className="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center"
+                  onClick={(e) => navigate('/employeeLogin')}
+                >
+                  Employee
+                </button>
+              </div>
+              <div className="text-center text-lg-start">
+                <button
+                  className="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center"
+                  onClick={(e) => navigate('/login')}
+                >
+                  Admin
+                </button>
+              </div>
+            </div>
+          </div>
+          <div
+            className="col-lg-6 hero-img"
+            data-aos="zoom-out"
+            data-aos-delay="200"
           >
-            Employee
-          </button>
-          <button
-            className="btn btn-success btn-lg"
-            onClick={(e) => navigate('/login')}
-          >
-            Admin
-          </button>
+            <img src="hero-img.svg" className="img-fluid" alt="" />
+          </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 }
 
